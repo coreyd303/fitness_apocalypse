3e59bedc5d62074b02b529d44bc813ce73cb4cd3
coreydavis <corey@commercekitchen.com>
2016-07-08 11:30:32 -0600

    [ CHG ] Update home page and nav bar Home page and nav bar have new styles applied as well as images

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
 
+  get 'login/new', as: :login
+
   resources :events, only: [:index, :show]
   resources :blog_posts, only: [:index, :show]
   resources :wods, only: [:show]
