1ef2d887b87d1d068c7cd8810d2997d39f82f132
coreydavis <corey@commercekitchen.com>
2016-07-08 13:09:09 -0600

    [ REFAC ] Change workflow for signin / up Users go to single page view for registrations and sign ins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :resource_name, :resource, :devise_mapping
+
+  protected
+
+
+  def resource_name
+   :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:name, :firstname, :lastname, :phone]
+    devise_parameter_sanitizer.for(:account_update) << [:name, :firstname, :lastname, :image, :phone]
+  end
+
 end
