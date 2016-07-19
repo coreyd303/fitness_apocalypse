0c5dfccd1c0b77b051744aa15f23918392b59933
coreydavis <corey@commercekitchen.com>
2016-07-19 16:31:45 -0600

    [ FEAT ] User sees daily WOD on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < BaseController
   def index
+    if current_user && Wod.count > 0
+      date_to_string = Time.zone.now.strftime("%m/%d/%Y")
+      @wod = Wod.find_by(workout_date: date_to_string)
+    end
+
+    @rest = "Rest and Recover today, if you're tired, relax, otherwise you can do a light recovery walk, strech, or ride."
   end
 end
